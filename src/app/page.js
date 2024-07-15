f73bb3784cdd280ad7b122183a9f7d1cdffc7b70
Etem Utku Oylum <utkuoylum@Etems-MacBook-Air.local>
2024-07-15 15:28:27 +0200

    home page

--- src/app/page.js
+++ src/app/page.js
@@ -4,6 +4,7 @@ import styles from "./page.module.css";
 import { useEffect, useState } from "react";
 import Preloader from "@/components/Preloader/Preloader";
 import { AnimatePresence } from "framer-motion";
+import GridLayout from "@/components/GridLayout/GridLayout";
 
 export default function Home() {
   const [isLoading, setIsLoading] = useState(true);
@@ -18,7 +19,7 @@ export default function Home() {
   return (
     <main className={styles.main}>
       <AnimatePresence mode="wait">{isLoading && <Preloader />}</AnimatePresence>
-      deneme
+      <GridLayout />
     </main>
   );
 }
---
+++ src/components/GridLayout/GridLayout.jsx
@@ -0,0 +1,11 @@
+import styles from './GridLayout.module.css'
+ 
+export default function GridLayout() {
+return (
+<div className={styles.container}>
+    <div className={styles.bio}></div>
+    <div className={styles.social}></div>
+    <div className={styles.stack}></div>
+    <div className={styles.tools}></div>
+</div>
+)}
