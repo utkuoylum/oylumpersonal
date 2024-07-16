5c515111b7c6e373ce59cad6ca6a5b5c673d8610
Etem Utku Oylum <utkuoylum@Etems-Air.speakeasy-bwest.local>
2024-07-16 19:53:36 +0200

    slider fix

--- src/app/layout.js
+++ src/app/layout.js
@@ -13,8 +13,9 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Navbar />
-        {children}</body>
+        
+        {children}
+        <Navbar /></body>
     </html>
   );
 }
