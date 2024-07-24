e5c63aeff2f49168e3f696f99e81220cc8723f79
Etem Utku Oylum <utkuoylum@Etems-MacBook-Air.local>
2024-07-24 21:51:30 +0200

    grid layout fix

--- src/app/page.js
+++ src/app/page.js
@@ -8,6 +8,7 @@ import GridLayout from "@/components/GridLayout/GridLayout";
 import SlidingCards from "@/components/SlidingCards/SlidingCards";
 import BioPage from "@/components/BioPage/BioPage";
 import LEDBoard from "@/components/Ledboard/Ledboard";
+import Button from "@/components/Button/Button";
 
 export default function Home() {
   const [isLoading, setIsLoading] = useState(true);
@@ -22,10 +23,17 @@ export default function Home() {
   return (
     <main className={styles.main}>
       <AnimatePresence mode="wait">{isLoading && <Preloader />}</AnimatePresence>
-      <BioPage />
+      {/* <BioPage /> */}
       {/* <GridLayout /> */}
       {/* <SlidingCards /> */}
       {/* <LEDBoard /> */}
+      <div className={styles.gridContainer}>
+        <div className={styles.gridCol}>
+          <Button />
+        </div>
+        <div className={styles.gridCol}></div>
+        <div className={styles.gridCol}></div>
+      </div>
       
     </main>
   );
---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,9 @@
+import styles from './Button.module.css'
+ 
+export default function Button() {
+return (
+<button className={styles.button57} role="button">
+  <span className={styles.text}>Button 57</span>
+  <span>Alternate text</span>
+</button>
+)}
