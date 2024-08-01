7f0b9a2aa72ac5c03b05fa51df81bb23208f3da4
Etem Utku Oylum <utkuoylum@Etems-MacBook-Air.local>
2024-08-01 20:12:58 +0200

    grid fix

--- src/components/GridLayout/GridLayout.jsx
+++ src/components/GridLayout/GridLayout.jsx
@@ -3,9 +3,9 @@ import styles from './GridLayout.module.css'
 export default function GridLayout() {
 return (
 <div className={styles.container}>
-    <div className={styles.bio}></div>
-    <div className={styles.social}></div>
-    <div className={styles.stack}></div>
-    <div className={styles.tools}></div>
+    <div className={styles.bio}>bio</div>
+    <div className={styles.social}>social</div>
+    <div className={styles.stack}>stack</div>
+    <div className={styles.tools}>tools</div>
 </div>
 )}
