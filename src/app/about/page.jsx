34a9fbaa863be873f2bdfe6ad3155de34d0e2334
Etem Utku Oylum <utkuoylum@Etems-MacBook-Air.local>
2024-08-17 21:50:37 +0200

    about page

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,7 +1,6 @@
-'use client'
-import React, { useState } from 'react';
-import styles from './About.module.css';
-import Avatar from '@/components/Avatar/Avatar';
+"use client";
+import React, { useState } from "react";
+import styles from "./About.module.css";
 
 // icons
 import {
@@ -11,22 +10,22 @@ import {
   FaReact,
   FaWordpress,
   FaFigma,
-} from 'react-icons/fa';
+} from "react-icons/fa";
 
 import {
   SiNextdotjs,
   SiFramer,
   SiAdobexd,
   SiAdobephotoshop,
-} from 'react-icons/si';
+} from "react-icons/si";
 
 //  about data
 export const aboutData = [
   {
-    title: 'skills',
+    title: "skills",
     info: [
       {
-        title: 'Web Development',
+        title: "Web Development",
         icons: [
           <FaHtml5 key="html5" />,
           <FaCss3 key="css3" />,
@@ -38,7 +37,7 @@ export const aboutData = [
         ],
       },
       {
-        title: 'UI/UX Design',
+        title: "UI/UX Design",
         icons: [
           <FaFigma key="figma" />,
           <SiAdobexd key="adobexd" />,
@@ -48,60 +47,61 @@ export const aboutData = [
     ],
   },
   {
-    title: 'awards',
+    title: "awards",
     info: [
       {
-        title: 'Webby Awards - Honoree',
-        stage: '2011 - 2012',
+        title: "Webby Awards - Honoree",
+        stage: "2011 - 2012",
       },
       {
-        title: 'Adobe Design Achievement Awards - Finalist',
-        stage: '2009 - 2010',
+        title: "Adobe Design Achievement Awards - Finalist",
+        stage: "2009 - 2010",
       },
     ],
   },
   {
-    title: 'experience',
+    title: "experience",
     info: [
       {
-        title: 'UX/UI Designer - XYZ Company',
-        stage: '2012 - 2023',
+        title: "UX/UI Designer - XYZ Company",
+        stage: "2012 - 2023",
       },
       {
-        title: 'Web Developer - ABC Agency',
-        stage: '2010 - 2012',
+        title: "Web Developer - ABC Agency",
+        stage: "2010 - 2012",
       },
       {
-        title: 'Intern - DEF Corporation',
-        stage: '2008 - 2010',
+        title: "Intern - DEF Corporation",
+        stage: "2008 - 2010",
       },
     ],
   },
   {
-    title: 'credentials',
+    title: "credentials",
     info: [
       {
-        title: 'Web Development - ABC University, LA, CA',
-        stage: '2011',
+        title: "Web Development - ABC University, LA, CA",
+        stage: "2011",
       },
       {
-        title: 'Computer Science Diploma - AV Technical Institute',
-        stage: '2009',
+        title: "Computer Science Diploma - AV Technical Institute",
+        stage: "2009",
       },
       {
-        title: 'Certified Graphic Designer - ABC Institute, Los Angeles, CA',
-        stage: '2006',
+        title: "Certified Graphic Designer - ABC Institute, Los Angeles, CA",
+        stage: "2006",
       },
     ],
   },
 ];
 
 // framer motion
-import { motion } from 'framer-motion';
-import { fadeIn } from '/variants';
+import { motion } from "framer-motion";
+import { fadeIn } from "/variants";
 
 // counter
-import CountUp from 'react-countup';
+import CountUp from "react-countup";
+import Image from "next/image";
 
 const About = () => {
   const [index, setIndex] = useState(0);
@@ -109,33 +109,26 @@ const About = () => {
   return (
     <div className={styles.aboutContainer}>
       {/* avatar img */}
-      <motion.div
-        variants={fadeIn('right', 0.2)}
-        initial='hidden'
-        animate='show'
-        exit='hidden'
-        className={styles.avatarContainer}
-      >
-        <Avatar />
-      </motion.div>
+
       <div className={styles.contentContainer}>
         {/* text */}
+        <div className={styles.blankStart}></div>
         <div className={styles.textContainer}>
           <motion.h2
-            variants={fadeIn('right', 0.2)}
-            initial='hidden'
-            animate='show'
-            exit='hidden'
+            variants={fadeIn("right", 0.2)}
+            initial="hidden"
+            animate="show"
+            exit="hidden"
             className={styles.heading}
           >
-            Captivating <span className={styles.headingAccent}>stories</span> birth
-            magnificent designs.
+            Captivating <span className={styles.headingAccent}>stories</span>{" "}
+            birth magnificent designs.
           </motion.h2>
           <motion.p
-            variants={fadeIn('right', 0.4)}
-            initial='hidden'
-            animate='show'
-            exit='hidden'
+            variants={fadeIn("right", 0.4)}
+            initial="hidden"
+            animate="show"
+            exit="hidden"
             className={styles.paragraph}
           >
             10 years ago, I began freelancing as a developer. Since then, I've
@@ -144,10 +137,10 @@ const About = () => {
           </motion.p>
           {/* counters */}
           <motion.div
-            variants={fadeIn('right', 0.6)}
-            initial='hidden'
-            animate='show'
-            exit='hidden'
+            variants={fadeIn("right", 0.6)}
+            initial="hidden"
+            animate="show"
+            exit="hidden"
             className={styles.counters}
           >
             <div className={styles.experience}>
@@ -156,83 +149,88 @@ const About = () => {
                 <div className={styles.counterText}>
                   <CountUp start={0} end={10} duration={5} /> +
                 </div>
-                <div className={styles.counterLabel}>
-                  Years of experience
-                </div>
+                <div className={styles.counterLabel}>Years of experience</div>
               </div>
               {/* clients */}
               <div className={styles.counterItem}>
                 <div className={styles.counterText}>
                   <CountUp start={0} end={250} duration={5} /> +
                 </div>
-                <div className={styles.counterLabel}>
-                  Satisfied clients
-                </div>
+                <div className={styles.counterLabel}>Satisfied clients</div>
               </div>
               {/* projects */}
               <div className={styles.counterItem}>
                 <div className={styles.counterText}>
                   <CountUp start={0} end={650} duration={5} /> +
                 </div>
-                <div className={styles.counterLabel}>
-                  Finished projects
-                </div>
+                <div className={styles.counterLabel}>Finished projects</div>
               </div>
               {/* awards */}
               <div className={styles.counterItem}>
                 <div className={styles.counterText}>
                   <CountUp start={0} end={8} duration={5} /> +
                 </div>
-                <div className={styles.counterLabel}>
-                  Winning awards
-                </div>
+                <div className={styles.counterLabel}>Winning awards</div>
               </div>
             </div>
           </motion.div>
         </div>
         {/* info */}
         <motion.div
-          variants={fadeIn('left', 0.4)}
-          initial='hidden'
-          animate='show'
-          exit='hidden'
+          variants={fadeIn("left", 0.4)}
+          initial="hidden"
+          animate="show"
+          exit="hidden"
           className={styles.infoContainer}
         >
-          <div className={styles.infoTabs}>
-            {aboutData.map((item, itemIndex) => {
-              return (
-                <div
-                  key={itemIndex}
-                  className={`${styles.infoTab} ${
-                    index === itemIndex ? styles.infoTabActive : ''
-                  }`}
-                  onClick={() => setIndex(itemIndex)}
-                >
-                  {item.title}
-                </div>
-              );
-            })}
-          </div>
-          <div className={styles.infoContent}>
-            {aboutData[index].info.map((item, itemIndex) => {
-              return (
-                <div
-                  key={itemIndex}
-                  className={styles.infoItem}
-                >
-                  {/* title */}
-                  <div className={styles.infoItemTitle}>{item.title}</div>
-                  <div className={styles.stage}>-</div>
-                  <div>{item.stage}</div>
-                  <div className={styles.infoIcons}>
-                    {/* icons */}
-                    {item.icons?.map((icon, itemIndex) => {
-                      return <div key={itemIndex} className={styles.infoIcon}>{icon}</div>;
-                    })}
+          <motion.div
+            variants={fadeIn("right", 0.2)}
+            initial="hidden"
+            animate="show"
+            exit="hidden"
+            className={styles.avatarContainer}
+          >
+            <Image src={"/nobgpp.png"} alt="" fill className={styles.image} />
+          </motion.div>
+          <div className={styles.detailsContainer}>
+            {" "}
+            <div className={styles.infoTabs}>
+              {aboutData.map((item, itemIndex) => {
+                return (
+                  <div
+                    key={itemIndex}
+                    className={`${styles.infoTab} ${
+                      index === itemIndex ? styles.infoTabActive : ""
+                    }`}
+                    onClick={() => setIndex(itemIndex)}
+                  >
+                    {item.title}
                   </div>
-                </div>
-              );
-            })}
+                );
+              })}
+            </div>
+            <div className={styles.infoContent}>
+              {aboutData[index].info.map((item, itemIndex) => {
+                return (
+                  <div key={itemIndex} className={styles.infoItem}>
+                    {/* title */}
+                    <div className={styles.infoItemTitle}>{item.title}</div>
+                    <div className={styles.stage}>-</div>
+                    <div>{item.stage}</div>
+                    <div className={styles.infoIcons}>
+                      {/* icons */}
+                      {item.icons?.map((icon, itemIndex) => {
+                        return (
+                          <div key={itemIndex} className={styles.infoIcon}>
+                            {icon}
+                          </div>
+                        );
+                      })}
+                    </div>
+                  </div>
+                );
+              })}
+            </div>
           </div>
         </motion.div>
       </div>
